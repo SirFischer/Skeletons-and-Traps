b837010fdbb971ad118c70215db5cab6aff8e1e9
Marek <marek5886@hotmail.com>
2020-11-29 01:56:12 +0100

    pretty frame addition

--- srcs/Game/Entities/Hero.cpp
+++ srcs/Game/Entities/Hero.cpp
@@ -3,14 +3,8 @@
 Hero::Hero(/* args */)
 {
 	mSprite.setTexture(*ResourceManager::LoadTexture("assets/textures/Hero.png"));
-	int animation_line = 2 * 64;
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(0, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * 2, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * 3, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * 4, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * 5, animation_line, 64, 64));
-	mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * 6, animation_line, 64, 64));
+	for (size_t i = 0; i < 7; i++)
+		mAnimations[Action::IDLE].AddFrame(sf::IntRect(64 * i, 128, 64, 64));
 	mAnimations[Action::IDLE].SetLength(0.1);
 }
 
